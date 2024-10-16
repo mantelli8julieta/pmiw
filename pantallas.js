da161a22249b5d8dfc3a821ebd7268c38fc258a1
mantelli8julieta <128554516+mantelli8julieta@users.noreply.github.com>
2024-10-16 20:26:58 -0300

    Add files via upload

--- pantallas.js
+++ pantallas.js
@@ -1,3 +1,30 @@
+function pantallaInicio() {
+  fill(51, 0, 15);
+  textFont(titulos);
+  textSize(50);
+  noStroke();
+  fill(250, 187, 68);
+  rect(132, 90, 390, 70);
+  fill(72, 25, 15);
+  text("RATATOUILLE", 140, 140);
+  textSize(25);
+  fill(170, 146, 131);
+  rect(200, 200, 250, 90, 10);
+  rect(200, 310, 250, 90, 10);
+  textFont(parrafos);
+  fill(51, 0, 15);
+  text("iniciar", 290, 255);
+  text("créditos", 280, 365);
+}
+
+function botonRESET() {
+  fill(64, 53, 33);
+  rect(273, 425, 100, 30);
+  fill(255);
+  text("RESET", 298, 445);
+}
+
+
 function cuadritoTexto() {
   fill(170, 146, 131);
   rect(20, 340, 600, 125, 10);
@@ -12,85 +39,103 @@ function botonContinuar() {
   triangle(575, 420, 575, 440, 590, 431);
 }
 
+function botonesDecisiones() {
+  noStroke();
+  fill(64, 53, 33);
+  rect(175, 425, 100, 30);
+  rect(370, 425, 100, 30);
+  fill(255);
+  text("[A]", 220, 445);
+  text("[B]", 413, 445);
+}
+
+
 function todasLasPantallas() {
   if (pantallaActual == 0) {
-    image(fondos[0], 0, 0, 640, 480);
-    botonContinuar();
+    image(fondos[pantallaActual], 0, 0, 640, 480);
+    pantallaInicio();
   }
 
   if (pantallaActual == 1) {
-    image(fondos[1], 0, 0, 640, 480);
-    botonContinuar();
+    image(fondos[pantallaActual], 0, 0, 640, 480);
   }
 
   if (pantallaActual == 2) {
-    image(fondos[2], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonesDecisiones();
   }
 
   if (pantallaActual == 3) {
-    image(fondos[3], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
     botonContinuar();
   }
 
   if (pantallaActual == 4) {
-    image(fondos[4], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonesDecisiones();
   }
+
   if (pantallaActual == 5) {
-    image(fondos[5], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonRESET();
   }
 
   if (pantallaActual == 6) {
-    image(fondos[6], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
     botonContinuar();
   }
 
   if (pantallaActual == 7) {
-    image(fondos[7], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
     botonContinuar();
   }
 
   if (pantallaActual == 8) {
-    image(fondos[8], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonesDecisiones();
   }
 
   if (pantallaActual == 9) {
-    image(fondos[9], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonRESET();
   }
 
   if (pantallaActual == 10) {
-    image(fondos[10], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonesDecisiones();
   }
 
   if (pantallaActual == 11) {
-    image(fondos[11], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
     botonContinuar();
   }
 
   if (pantallaActual == 12) {
-    image(fondos[12], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonesDecisiones();
   }
 
   if (pantallaActual == 13) {
-    image(fondos[13], 0, 0, 640, 480);
+    image(fondos[pantallaActual], 0, 0, 640, 480);
     cuadritoTexto();
-    botonContinuar();
+    botonRESET();
+  }
+
+
+  if (pantallaActual == 14) {
+    image(fondos[pantallaActual], 0, 0, 640, 480);
+    cuadritoTexto();
+    botonRESET();
   }
 }
--- tpfinalparte1.js
+++ tpfinalparte1.js
@@ -11,17 +11,13 @@ let fondos = [];
 
 function setup() {
   createCanvas(640, 480);
-  /*hay q crear ciclos for para:
-   1- la carga de imágenes
-   2- para cargar strings
-   3-
-   */
 }
 
 function preload() {
   texto = loadStrings("assets/parrafos.txt");
   titulos = loadFont("assets/fuentes/Corben-Regular.ttf");
   parrafos = loadFont("assets/fuentes/RobotoSlab.ttf");
+  
   for (let num = 0; num < pantallasTotales; num+=1) {
     fondos[num] = loadImage("/assets/imgs/img" + num + ".jpg");
   }
@@ -30,18 +26,13 @@ function preload() {
 
 function draw() {
   background(201, 198, 179);
-  fill(51, 0, 15);
-  textFont(titulos);
-  textSize(50);
-  text("RATATOUILLE", 140, 240);
-  textSize(25);
-  textFont(parrafos);
-  text("parrafo esto es un parrafo mira q lindo parrafo!", 100, 275);
   cuadritoTexto();
   todasLasPantallas();
 
-  print("mouseX es " + mouseX);
-  print("mouseY es " + mouseY);
+print("pantalla es " + pantallaActual);
+  
+  /*print("mouseX es " + mouseX);
+  print("mouseY es " + mouseY);*/
   
 
 }
